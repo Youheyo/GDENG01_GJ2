6b6afc6f09f2d77300578f26056adff604a62fa4
Youheyo <80012381+Youheyo@users.noreply.github.com>
2022-11-29 23:34:55 +0800

    House with textures and openable doors

--- Assets/Scripts/Player/Interactables/ObjectInteracted.cs
+++ Assets/Scripts/Player/Interactables/ObjectInteracted.cs
@@ -5,29 +5,8 @@ using UnityEngine;
 public class ObjectInteracted : MonoBehaviour
 {
 
-	[SerializeField] private GameObject affectedObject;
-	[SerializeField] private string objName;
-
-	public void onInteract() {
-
+	virtual public void onInteract() {
+		
 	}
 
-	void Awake()
-	{
-		if(objName.Length <= 0) {
-			objName = this.name;
-		}
-	}
-
-	private void OnDestroy()
-	{
-	// Either replace with RemoveAllObserver (or whatever that sounded familiar with it
-	// or refactor so that observers could be removed properly.
-	//EventBroadcaster.Instance.RemoveAllObservers();
-	}
-	
-	private void disableAffectedObject()
-	{
-		affectedObject.SetActive(!affectedObject.activeSelf);
-	}
 }
---
+++ Assets/Scripts/Player/ObjectScripts/DoorScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : ObjectInteracted
+{
+	private bool isOpen = false;
+	[SerializeField] Animator animator;
+
+
+	public override void onInteract()
+	{
+		if (isOpen)
+		{
+			isOpen = !isOpen;
+			animator.SetBool("isOpen", false);
+		}
+		else
+		{
+			isOpen = !isOpen;
+			animator.SetBool("isOpen", true);
+		}
+	}
+
+}
