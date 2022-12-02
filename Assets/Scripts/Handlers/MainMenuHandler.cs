e77d056e2a62e7cfe358bbe104a8e53783e52f11
Youheyo <80012381+Youheyo@users.noreply.github.com>
2022-12-02 22:59:50 +0800

    Added a help panel to learn controls and simple goal

--- Assets/Scripts/Handlers/MainMenuHandler.cs
+++ Assets/Scripts/Handlers/MainMenuHandler.cs
@@ -5,9 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
 {
+
+	public GameObject HelpPanel;
+
     public void playBtn() {
 	SceneManager.LoadScene("Game");
     }
+
+	public void helpBtn()
+	{
+		HelpPanel.SetActive(true);
+	}
+	public void exitHelpPanel()
+	{
+		HelpPanel.SetActive(false);
+	}
 
     public void quitBtn() {
 	Application.Quit();
